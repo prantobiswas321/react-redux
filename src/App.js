3d4da9db944675317a62d3df44b2a43e4e4d3121
prantobiswas321 <rahulbd321@gmail.com>
2021-12-23 17:09:11 +0600

    components done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
+import ProductSingle from './components/ProductSingle';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/ProductSingle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductSingle = () => {
+    return (
+        <div>
+            <h1>Product Single</h1>
+        </div>
+    );
+};
+
+export default ProductSingle;
