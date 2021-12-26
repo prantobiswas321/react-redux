ee462a3fcc2a9033d8cea172e4d6ea6ab0f65333
prantobiswas321 <rahulbd321@gmail.com>
2021-12-26 23:11:38 +0600

    product detail done

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
-import { productReducers } from "./productReducer";
+import { productReducers, selectedProductReducer } from "./productReducer";
 
 const reducers = combineReducers({
     allProducts: productReducers,
+    product: selectedProductReducer,
 });
 
 export default reducers; 
