0cdd17953e71da2b4c83bf6f0897793c7cdae09f
prantobiswas321 <rahulbd321@gmail.com>
2021-12-30 22:38:03 +0600

    done

--- src/components/ShowFourPoduct.js
+++ src/components/ShowFourPoduct.js
@@ -4,19 +4,24 @@ import { Link } from 'react-router-dom';
 const ShowFourPoduct = (props) => {
     const { id, title, image, price, category } = props.product;
     return (
-        <div className="col">
-            <div className="card h-100">
-                <img src={image} className="mx-auto my-3 card-img-top" style={{ width: "220px", height: "320px" }} alt={title} />
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <h4>$ {price}</h4>
-                    <h4>{category}</h4>
-                </div>
-                <Link to={`/product/${id}`}>
-                    <button className="my-3 btn btn-success">View Details</button>
-                </Link>
+        <div>
+            <div>
+                <h1>Feature Products</h1>
             </div>
-        </div >
+            <div className="col">
+                <div className="card h-100">
+                    <img src={image} className="mx-auto my-3 card-img-top" style={{ width: "220px", height: "320px" }} alt={title} />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <h4>$ {price}</h4>
+                        <h4>{category}</h4>
+                    </div>
+                    <Link to={`/product/${id}`}>
+                        <button className="my-3 btn btn-success">View Details</button>
+                    </Link>
+                </div>
+            </div >
+        </div>
     );
 };
 
