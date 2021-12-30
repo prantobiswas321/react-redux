e1c7d50f77a866ae15f27163aec6184963cec8c9
prantobiswas321 <rahulbd321@gmail.com>
2021-12-30 23:12:43 +0600

    loading spinner added

--- src/components/FourServices.js
+++ src/components/FourServices.js
@@ -9,12 +9,27 @@ const FourServices = () => {
     const result = products.slice(0, size);
     console.log(result);
     return (
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+        <div>
             {
-                result.map((product, key) => <ShowFourPoduct
-                    key={key}
-                    product={product}
-                ></ShowFourPoduct>)
+                result.length === 0 ? <div class="d-flex justify-content-center">
+                    <div class="spinner-border" role="status">
+                        <span class="sr-only"></span>
+                    </div>
+                </div>
+                    :
+                    <div>
+                        <div className="mb-5">
+                            <h1>Feature Products</h1>
+                        </div>
+                        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+                            {
+                                result.map((product, key) => <ShowFourPoduct
+                                    key={key}
+                                    product={product}
+                                ></ShowFourPoduct>)
+                            }
+                        </div>
+                    </div>
             }
         </div>
     );
--- src/components/ShowFourPoduct.js
+++ src/components/ShowFourPoduct.js
@@ -4,24 +4,21 @@ import { Link } from 'react-router-dom';
 const ShowFourPoduct = (props) => {
     const { id, title, image, price, category } = props.product;
     return (
-        <div>
-            <div>
-                <h1>Feature Products</h1>
-            </div>
-            <div className="col">
-                <div className="card h-100">
-                    <img src={image} className="mx-auto my-3 card-img-top" style={{ width: "220px", height: "320px" }} alt={title} />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <h4>$ {price}</h4>
-                        <h4>{category}</h4>
-                    </div>
-                    <Link to={`/product/${id}`}>
-                        <button className="my-3 btn btn-success">View Details</button>
-                    </Link>
+
+        <div className="col">
+            <div className="card h-100">
+                <img src={image} className="mx-auto my-3 card-img-top" style={{ width: "220px", height: "320px" }} alt={title} />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <h4>$ {price}</h4>
+                    <h4>{category}</h4>
                 </div>
-            </div >
-        </div>
+                <Link to={`/product/${id}`}>
+                    <button className="my-3 btn btn-success">View Details</button>
+                </Link>
+            </div>
+        </div >
+
     );
 };
 
